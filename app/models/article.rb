925ec149e88bead79f11fa0ac054b1b62220facd
genkamata <kamagen0426@gmail.com>
2015-08-22 13:56:40 +0000

    20150824

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ActiveRecord::Base
-  has_many :image, :dependent => :destroy
+ 
   has_attached_file :image,   
-      :styles => { large: "640x400>",  medium:"200x200", thumb: "100x100>" }, # 画像サイズを指定
+      :styles => { large: "1280x768>",  medium:"200x200", thumb: "100x100>" }, # 画像サイズを指定
       :url  => "/assets/arts/:id/:style/:basename.:extension", # 画像保存先のURL先
       :path => "#{Rails.root}/public/assets/arts/:id/:style/:basename.:extension" # サーバ上の画像保存先パス
 
